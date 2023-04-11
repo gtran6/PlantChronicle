99917ba18986fc913aa1a2359457f28ff071c4f9
gtran6 <gtran6@depaul.edu>
2023-04-11 18:13:49 -0500

    finish redbud unit test with mock data

--- app/src/main/java/com/myprojects/plantchronicle/ui/main/MainViewModel.kt
+++ app/src/main/java/com/myprojects/plantchronicle/ui/main/MainViewModel.kt
@@ -1,7 +1,14 @@
 package com.myprojects.plantchronicle.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.myprojects.plantchronicle.dto.Plant
+import com.myprojects.plantchronicle.service.PlantService
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var plants: MutableLiveData<ArrayList<Plant>> = MutableLiveData<ArrayList<Plant>>()
+    var plantService: PlantService = PlantService()
+    fun fetchPlants(plantName: String) {
+        plants = plantService.fetchPlants(plantName)
+    }
 }
--- app/src/test/java/com/myprojects/plantchronicle/PlantDataUnitTest.kt
+++ app/src/test/java/com/myprojects/plantchronicle/PlantDataUnitTest.kt
@@ -1,8 +1,12 @@
 package com.myprojects.plantchronicle
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
 import com.myprojects.plantchronicle.dto.Plant
+import com.myprojects.plantchronicle.service.PlantService
 import com.myprojects.plantchronicle.ui.main.MainViewModel
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -19,22 +23,42 @@ class PlantDataUnitTest {
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
     lateinit var mvm: MainViewModel
+    var plantService = mockk<PlantService>()
 
     @Test
-    fun confirmEasternRedbud_outputsEasternRedbud() {
-        var plant = Plant("Cercis", "canadensis", "Eastern Redbud")
+    fun confirmEasternRedbud_outputsEasternRedbud () {
+        var plant: Plant = Plant("Cercis", "canadesis", "Eastern Redbud")
         assertEquals("Eastern Redbud", plant.toString())
     }
 
     @Test
     fun searchForRedbud_returnsRedbud() {
-        givenAFeedPlantDataAreAvailable()
+        givenAFeedOfMockedPlantDataAreAvailable()
         whenSearchForRedbud()
         thenResultContainsEasternRedbud()
     }
 
-    private fun givenAFeedPlantDataAreAvailable() {
+    private fun givenAFeedOfMockedPlantDataAreAvailable() {
         mvm = MainViewModel()
+        createMockData()
+    }
+
+    private fun createMockData() {
+        var allPlantsLiveData = MutableLiveData<ArrayList<Plant>>()
+        var allPlants = ArrayList<Plant>()
+        // create and add plants to our collection.
+        var redbud = Plant("Cercis", "canadensis", "Eastern Redbud")
+        allPlants.add(redbud)
+        var redOak = Plant("Quercus", "rubra", "Red Oak")
+        allPlants.add(redOak)
+        var whiteOak = Plant("Quercus", "alba", "White Oak")
+        allPlants.add(whiteOak)
+        allPlantsLiveData.postValue(allPlants)
+
+        every {plantService.fetchPlants(any<String>())} returns allPlantsLiveData
+        every {plantService.fetchPlants(not(or("Redbud", "Quercus")))} returns MutableLiveData<ArrayList<Plant>>()
+
+        mvm.plantService = plantService
     }
 
     private fun whenSearchForRedbud() {
@@ -42,11 +66,11 @@ class PlantDataUnitTest {
     }
 
     private fun thenResultContainsEasternRedbud() {
-        var redbudFound = false
+        var redbudFound = false;
         mvm.plants.observeForever {
-            // here is where we do the observing
+            //here is where we do the observing
             assertNotNull(it)
-            assertTrue(it.size > 0) // not empty
+            assertTrue(it.size > 0)
             it.forEach {
                 if (it.genus == "Cercis" && it.species == "canadensis" && it.common.contains("Eastern Redbud")) {
                     redbudFound = true
@@ -58,7 +82,7 @@ class PlantDataUnitTest {
 
     @Test
     fun searchForGarbage_returnsNothing() {
-        givenAFeedPlantDataAreAvailable()
+        givenAFeedOfMockedPlantDataAreAvailable()
         whenISearchForGarbage()
         thenIGetZeroResults()
     }
