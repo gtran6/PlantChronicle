bf23f7d4cc6680ad0c47ccd33f5c56c8c35675a1
gtran6 <gtran6@depaul.edu>
2023-04-11 19:10:42 -0500

    create integration test

---
+++ app/src/main/java/com/myprojects/plantchronicle/dao/IPlaintDAO.kt
@@ -0,0 +1,17 @@
+package com.myprojects.plantchronicle.dao
+
+import com.myprojects.plantchronicle.dto.Plant
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface IPlaintDAO {
+
+    @GET("/perl/mobile/viewplantsjson.pl")
+    fun getAllPlants(): Call<ArrayList<Plant>>
+
+    @GET("/perl/mobile/viewplantsjson.pl")
+    fun getPlants(
+        @Query("Combined_Name") plantName: String
+    ) : Call<ArrayList<Plant>>
+}
--- app/src/main/java/com/myprojects/plantchronicle/service/PlantService.kt
+++ app/src/main/java/com/myprojects/plantchronicle/service/PlantService.kt
@@ -1,11 +1,31 @@
 package com.myprojects.plantchronicle.service
 
 import androidx.lifecycle.MutableLiveData
+import com.myprojects.plantchronicle.dao.IPlaintDAO
 import com.myprojects.plantchronicle.dto.Plant
+import com.myprojects.plantchronicle.extra.RetrofitClientInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PlantService {
 
     fun fetchPlants(plantName: String) : MutableLiveData<ArrayList<Plant>> {
-        return MutableLiveData<ArrayList<Plant>>()
+        var _plant = MutableLiveData<ArrayList<Plant>>()
+        val service = RetrofitClientInstance.retrofitInstance?.create(IPlaintDAO::class.java)
+        val call = service?.getAllPlants()
+        call?.enqueue(object : Callback<ArrayList<Plant>> {
+            override fun onResponse(
+                call: Call<ArrayList<Plant>>,
+                response: Response<ArrayList<Plant>>
+            ) {
+                _plant.value = response.body()
+            }
+
+            override fun onFailure(call: Call<ArrayList<Plant>>, t: Throwable) {
+            }
+        })
+
+        return _plant
     }
 }
